3ad25968c504bb0f46b45f311d3f10e983cd808c
Nikita Skryabin <nekit2271@gmail.com>
2024-10-06 18:36:08 +0300

    feat(ipregion.sh): add support for ipapi.com lookup

--- ipregion.sh
+++ ipregion.sh
@@ -26,6 +26,12 @@ ipregistry_co_lookup() {
   curl -s "https://api.ipregistry.co/$ip?hostname=true&key=$api_key" -H "Origin: https://ipregistry.co" | jq ".location.country.code"
 }
 
+ipapi_com_lookup() {
+  ip="$1"
+  curl -s "https://ipapi.com/ip_api.php?ip=$ip" | jq ".country_code"
+}
+
 ripe_rdap_lookup "$1"
 ipinfo_io_lookup "$1"
 ipregistry_co_lookup "$1"
+ipapi_com_lookup "$1"
