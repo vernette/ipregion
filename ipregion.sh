763dfd547b2ded7fb29c36902bcd7726dfdeed6c
Nikita Skryabin <nekit2271@gmail.com>
2024-10-06 18:32:52 +0300

    feat(ipregion.sh): add script shebang and description

---
+++ ipregion.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# Shell script to check IP contry code from various sources
+# curl and jq are required to run this script
+# 
+# Currently supported sources:
+#   - https://rdap.db.ripe.net
+#   - https://ipinfo.io
+#   - https://ipregistry.co
+#   - https://ipapi.com
