8ce4a427d05336db3d98834699f0daa369ddbf2a
Nikita Skryabin <nekit2271@gmail.com>
2024-10-06 18:33:56 +0300

    feat(ipregion.sh): add support for rdap.db.ripe.net lookup

--- ipregion.sh
+++ ipregion.sh
@@ -8,3 +8,10 @@
 #   - https://ipinfo.io
 #   - https://ipregistry.co
 #   - https://ipapi.com
+
+ripe_rdap_lookup() {
+  ip="$1"
+  curl -s https://rdap.db.ripe.net/ip/"$ip" | jq ".country"
+}
+
+ripe_rdap_lookup "$1"
